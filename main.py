b0a1de49e7d7bc534e9cfd8e8ea87ab52b12c737
Mob-Anish <mobanish16@gmail.com>
2026-01-17 22:24:22 +0545

    query params testing in fastapi added

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get('/')
 async def read_root():
     return {"message": "Hello world"}
-    
+
+
+@app.get('/greet')
+async def greet_name(name: str, age: int) -> dict:
+    return {"message": f"Hello {name}", "age": age}
