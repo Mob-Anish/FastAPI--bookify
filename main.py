2c40aa0b92c46fb69cc4b196effbfe1b5cf8e7ed
Mob-Anish <mobanish16@gmail.com>
2026-01-19 13:56:05 +0545

    model and get headers testing added

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+
+class BookCreateModel(BaseModel):
+    title: str
+    author: str
 
 
 @app.get('/')
@@ -9,5 +16,23 @@ async def read_root():
 
 
 @app.get('/greet')
-async def greet_name(name: str, age: int) -> dict:
+async def greet_name(name: Optional[str] = 'User', age: int = 0) -> dict:
     return {"message": f"Hello {name}", "age": age}
+
+
+@app.post('/create_book')
+async def create_book(book_data: BookCreateModel):
+    return {
+        "title": book_data.title,
+        "author": book_data.author
+    }
+
+
+@app.get('/get_headers')
+async def get_headers(accept: str = Header(None), content_type: str = Header(None), user_agent: str = Header(None)):
+    request_headers = {}
+    request_headers['Accept'] = accept
+    request_headers['Content-Type'] = content_type
+    request_headers['User-Agent'] = content_type
+
+    return request_headers
