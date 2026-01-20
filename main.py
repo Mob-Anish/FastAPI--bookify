9c94a3757886b674daed38e7dbfd323212a79095
Mob-Anish <mobanish16@gmail.com>
2026-01-20 12:56:10 +0545

    mocking crud api of books added

--- main.py
+++ main.py
@@ -4,35 +4,75 @@ from typing import Optional
 
 app = FastAPI()
 
-
-class BookCreateModel(BaseModel):
-    title: str
-    author: str
-
-
-@app.get('/')
-async def read_root():
-    return {"message": "Hello world"}
-
-
-@app.get('/greet')
-async def greet_name(name: Optional[str] = 'User', age: int = 0) -> dict:
-    return {"message": f"Hello {name}", "age": age}
-
-
-@app.post('/create_book')
-async def create_book(book_data: BookCreateModel):
-    return {
-        "title": book_data.title,
-        "author": book_data.author
+books = [
+    {
+        "id": 1,
+        "title": "The Art of Clean Code",
+        "author": "Robert C. Martin",
+        "publisher": "Prentice Hall",
+        "published_date": "2008-08-01",
+        "page_count": 464,
+        "language": "English"
+    },
+    {
+        "id": 2,
+        "title": "Learning Python the Hard Way",
+        "author": "Zed A. Shaw",
+        "publisher": "O'Reilly Media",
+        "published_date": "2017-06-30",
+        "page_count": 320,
+        "language": "English"
+    },
+    {
+        "id": 3,
+        "title": "JavaScript: The Definitive Guide",
+        "author": "David Flanagan",
+        "publisher": "O'Reilly Media",
+        "published_date": "2020-05-08",
+        "page_count": 706,
+        "language": "English"
+    },
+    {
+        "id": 4,
+        "title": "Introduction to Algorithms",
+        "author": "Thomas H. Cormen",
+        "publisher": "MIT Press",
+        "published_date": "2009-07-31",
+        "page_count": 1312,
+        "language": "English"
+    },
+    {
+        "id": 5,
+        "title": "You Don’t Know JS Yet",
+        "author": "Kyle Simpson",
+        "publisher": "Independently Published",
+        "published_date": "2020-01-28",
+        "page_count": 143,
+        "language": "English"
     }
+]
 
 
-@app.get('/get_headers')
-async def get_headers(accept: str = Header(None), content_type: str = Header(None), user_agent: str = Header(None)):
-    request_headers = {}
-    request_headers['Accept'] = accept
-    request_headers['Content-Type'] = content_type
-    request_headers['User-Agent'] = content_type
+@app.get('/books')
+async def get_all_books() -> list:
+    pass
 
-    return request_headers
+
+@app.get('/books')
+async def create_book() -> dict:
+    pass
+
+
+@app.get('/book/{book_id}')
+async def get_book(book_id: int) -> dict:
+    pass
+
+
+@app.get('/book/{book_id}')
+async def update_book(book_id: int) -> dict:
+    pass
+
+
+@app.get('/book/{book_id}')
+async def delete_book(book_id: int) -> dict:
+    pass
