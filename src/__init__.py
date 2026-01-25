37c77929f00718663491efbfd9ad0ed65027fe80
Mob-Anish <mobanish16@gmail.com>
2026-01-25 21:08:12 +0545

    database connection using sqlalchemy and postgresql and environment setup added

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import init_db
+
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print("Server is starting ...")
+    await init_db()
+    try:
+        yield
+    finally:
+        # Teardown phase
+        print("Server has been stopped...")
+
 
 version = 'v1'
 
 app = FastAPI(title='Bookify',
-              description='RestAPI using fastapi', version=version)
+              description='RestAPI using fastapi', version=version,
+              lifespan=life_span)
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=['books'])
